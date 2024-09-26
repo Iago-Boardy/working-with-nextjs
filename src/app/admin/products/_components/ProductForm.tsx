4a46175788a9f64f8470c90c98a7dc3ffbe093b5
Iago Boardy <iagogol2006@gmail.com>
2024-09-26 19:21:07 -0300

    Removing Downloads Option

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -11,64 +11,61 @@ import { useFormState, useFormStatus } from "react-dom"
 import { Product } from "@prisma/client"
 import Image from "next/image"
 
-export function ProductForm({ product}: {product ?: Product | null}) {
+export function ProductForm({ product }: { product?: Product | null }) {
   const [error, action] = useFormState(
     product ? updateProduct.bind(null, product.id) : addProduct,
     {}
   );
-  const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents) 
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents);
 
-  return <>
-  <form action={action} className="space-y-8">
-    <div className="space-y-2">
-      <Label htmlFor="name">Nome</Label>
-      <Input type="text" id="name" name="name" defaultValue={product?.name || ""}/>
-      {error.name && <div className="text-destructive">{error.name}</div>}
-    </div>
+  return (
+    <>
+      <form action={action} className="space-y-8">
+        <div className="space-y-2">
+          <Label htmlFor="name">Nome</Label>
+          <Input type="text" id="name" name="name" defaultValue={product?.name || ""} />
+          {error.name && <div className="text-destructive">{error.name}</div>}
+        </div>
 
-    <div className="space-y-2">
-      <Label htmlFor="priceInCents">Preço pago em centavos</Label>
-      <Input type="number" id="priceInCents" name="priceInCents" value={priceInCents} onChange={e => setPriceInCents(Number(e.target.value) || undefined )}/>
-      <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
-      {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
-    </div>
+        <div className="space-y-2">
+          <Label htmlFor="priceInCents">Preço pago em centavos</Label>
+          <Input
+            type="number"
+            id="priceInCents"
+            name="priceInCents"
+            value={priceInCents}
+            onChange={e => setPriceInCents(Number(e.target.value) || undefined)}
+          />
+          <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
+          {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
+        </div>
 
-    <div className="space-y-2">
-      <Label htmlFor="description">Descrição</Label>
-      <Textarea id="description" name="description" defaultValue={product?.description || ""}/>
-      {error.description && <div className="text-destructive">{error.description}</div>}
-    </div>
+        <div className="space-y-2">
+          <Label htmlFor="description">Descrição</Label>
+          <Textarea id="description" name="description" defaultValue={product?.description || ""} />
+          {error.description && <div className="text-destructive">{error.description}</div>}
+        </div>
 
-    <div className="space-y-2">
-      <Label htmlFor="file">Arquivo</Label>
-      <Input type="file" id="file" name="file" required={product == null}/>
-      {product != null && <div className="text-muted-foreground">{product.filePath}</div>}
-      {error.file && <div className="text-destructive">{error.file}</div>}
-    </div>
+        <div className="space-y-2">
+          <Label htmlFor="image">Imagem</Label>
+          <Input type="file" id="image" name="image" required={product == null} />
+          {product != null && (
+            <Image src={product.imagePath} height="400" width="400" alt="product-image" />
+          )}
+          {error.image && <div className="text-destructive">{error.image}</div>}
+        </div>
 
-    <div className="space-y-2">
-      <Label htmlFor="image">Imagem</Label>
-      <Input type="file" id="image" name="image" required={product == null}/>
-
-      {product != null && <Image 
-      src={product.imagePath} 
-      height="400" 
-      width="400" 
-      alt="product-image" />}
-
-      {error.image && <div className="text-destructive">{error.image}</div>}
-    </div>
-    <SubmitButton/>
-  </form>
-  
-  </>
+        <SubmitButton />
+      </form>
+    </>
+  );
 }
 
 function SubmitButton() {
   const { pending } = useFormStatus();
-  
+
   return (
-    <Button type="submit" disabled={pending}>  
+    <Button type="submit" disabled={pending}>
       {pending ? "Salvando..." : "Salvar"}
     </Button>
   );
