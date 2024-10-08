49150a55eb9f4a41dd2bd86c3710dcc5095aa4a7
Iago Boardy <iagogol2006@gmail.com>
2024-10-07 21:15:11 -0300

    Commiting now that the project is private

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,10 +1,15 @@
 import db from "@/db/db"
 import { notFound } from "next/navigation"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage( { params: { id}} : {params: {id: string}}) {
   const product = await db.product.findUnique({
   where: {id} })
   if (product == null) return notFound();
+
+  stripe.paymentIntents.create
 
   return <h1>Hi Bro</h1>
 }
